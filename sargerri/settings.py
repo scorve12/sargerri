6eefc5c6c121438c52da48571e3f9b2aae1deabf
scorve12 <scorve5322@gmail.com>
2025-09-19 01:37:13 +0900

    feat: update ALLOWED_HOSTS to allow all hosts

--- sargerri/settings.py
+++ sargerri/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o=6ojcw7^0=sqwv6u@d73ic$-!z%m&jx(^9*+zt!oi28)gqp@3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
